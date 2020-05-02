a9948d138f64d27a4d096e911f28592b0523168a
Zaigham Tahir <zaigtahir@gmail.com>
2020-05-01 23:50:50 -0500

    updated info screen and app icons

--- Medical Terminology/Controller/Info/InfoVC.swift
+++ Medical Terminology/Controller/Info/InfoVC.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import MessageUI
 
-class InfoVC: UIViewController {
+
+class InfoVC: UIViewController, MFMailComposeViewControllerDelegate {
 
     @IBOutlet weak var feedbackLabel: UILabel!
     @IBOutlet weak var feebackButton: UIButton!
@@ -16,10 +18,39 @@ class InfoVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        
-        feedbackLabel.text = "Your feedback is very important, and helps us improve this app. Do you have a suggestion, find an error, or have a request for a feature? Please take a few minutes to give us your feedback. Thank you.\n\nContact Email: myemail@gmail.com"
         
     }
-
+    
+    //MARK:- delegate functions
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func feedbackButtonAction(_ sender: Any) {
+        if let url = URL(string: "https://theappgalaxy.com") {
+            UIApplication.shared.open(url)
+        }
+        
+    }
+    
+    @IBAction func emailSupportButtonAction(_ sender: Any) {
+    
+        guard MFMailComposeViewController.canSendMail() else {
+            //can't sent email with this device
+            let alert = UIAlertController(title: "Email Issue", message: "It looks like you can not send an email on this device using this link. You can try sending an email directly to support@theappgalaxy.com using your email program", preferredStyle: .alert)
+            let alertActionOkay = UIAlertAction(title: "Okay", style: .default, handler: nil)
+            alert.addAction(alertActionOkay)
+            present(alert, animated: true)
+            return
+        }
+        
+        let composer = MFMailComposeViewController()
+        composer.mailComposeDelegate = self
+        composer.setToRecipients(["zaigtahir@gmail.com"])
+        composer.setSubject("Support request")
+        composer.setMessageBody("Hey, I got some feedback for you...", isHTML: false)
+        
+        present(composer, animated: true)
+    
+    }
 }
