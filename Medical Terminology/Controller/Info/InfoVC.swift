f4e792283fe86d391d5bba1ba5ac1184dc86cf24
Zaigham Tahir <zaigtahir@gmail.com>
2020-05-08 11:19:45 -0500

    before fixing list cell layout

--- Medical Terminology/Controller/Info/InfoVC.swift
+++ Medical Terminology/Controller/Info/InfoVC.swift
@@ -10,17 +10,13 @@ import UIKit
 import MessageUI
 
 
-class InfoVC: UIViewController, MFMailComposeViewControllerDelegate {
+class InfoVC: UIViewController {
     
     @IBOutlet weak var showScreenSwitch: UISwitch!
     @IBOutlet weak var seeWebsiteButton: UIButton!
-    @IBOutlet weak var emailButton: UIButton!
-    @IBOutlet weak var copyrightLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        emailButton.layer.cornerRadius = myConstants.button_cornerRadius
         
         seeWebsiteButton.layer.cornerRadius = myConstants.button_cornerRadius
         
@@ -34,12 +30,7 @@ class InfoVC: UIViewController, MFMailComposeViewControllerDelegate {
         }
         
     }
-    
-    //MARK:- delegate functions
-    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
-        controller.dismiss(animated: true, completion: nil)
-    }
-    
+        
     @IBAction func showWelcomeScreenSwitchAction(_ sender: UISwitch) {
         
         var showScreen = 0
@@ -56,32 +47,6 @@ class InfoVC: UIViewController, MFMailComposeViewControllerDelegate {
         if let url = URL(string: myConstants.appWebsite) {
             UIApplication.shared.open(url)
         }
-        
-    }
-    
-    @IBAction func showCompanyWebsiteButtonAction(_ sender: Any) {
-        if let url = URL(string: myConstants.companyWebsite) {
-                   UIApplication.shared.open(url)
-               }
-    }
-    @IBAction func emailSupportButtonAction(_ sender: Any) {
-        
-        guard MFMailComposeViewController.canSendMail() else {
-            //can't sent email with this device
-            let alert = UIAlertController(title: "Email Issue", message: "It looks like you can not send an email on this device using this link. You can try sending an email directly to support@theappgalaxy.com using your email program", preferredStyle: .alert)
-            let alertActionOkay = UIAlertAction(title: "Okay", style: .default, handler: nil)
-            alert.addAction(alertActionOkay)
-            present(alert, animated: true)
-            return
-        }
-        
-        let composer = MFMailComposeViewController()
-        composer.mailComposeDelegate = self
-        composer.setToRecipients(["zaigtahir@gmail.com"])
-        composer.setSubject("Support request")
-        composer.setMessageBody("Hey, I got some feedback for you...", isHTML: false)
-        
-        present(composer, animated: true)
         
     }
 }
