612d9a024def7ae586389c94e87fa25de8839742
Zaigham Tahir <zaigtahir@gmail.com>
2021-03-23 09:38:00 -0400

    added Terms and Term Controller

---
+++ Medical Terminology/Controllers/New Controllers/Term.swift
@@ -0,0 +1,40 @@
+//
+//  Term.swift
+//  Medical Terminology
+//
+//  Created by Zaigham Tahir on 3/23/21.
+//  Copyright © 2021 Zaigham Tahir. All rights reserved.
+//
+
+import Foundation
+
+class Term {
+	var termID: Int = -1
+	var name: String = "default"
+	var definition: String = "default"
+	var example: String = "default"
+	var audioFile: String?
+	var isFavorite: Bool = false
+	var isMyTerm: Bool = false
+	
+	init () {
+	
+	}
+	
+	convenience init(termID: Int, name: String, definition: String, audioFile: String?, isFavorite: Bool, isMyTerm: Bool) {
+		
+		self.init()
+		self.termID = termID
+		self.name = name
+		self.definition = definition
+		self.example = example
+		self.audioFile = audioFile
+		self.isFavorite = isFavorite
+		self.isMyTerm = isMyTerm
+	}
+	
+	func printTerm() {
+		print("term name: \(self.name)")
+		print("term ID: \(self.termID)")
+	}
+}
---
+++ Medical Terminology/Controllers/New Controllers/TermController.swift
@@ -0,0 +1,19 @@
+//
+//  TermController.swift
+//  Medical Terminology
+//
+//  Created by Zaigham Tahir on 3/23/21.
+//  Copyright © 2021 Zaigham Tahir. All rights reserved.
+//
+
+import Foundation
+
+class TermController {
+	
+	func getTerms (termID: Int) {
+		
+		let query = "SELECT termID FROM \()"
+		
+	}
+	
+}
