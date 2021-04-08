df29713a8bcf9011aec98e46607ce0271f88c1b9
Zaigham Tahir <zaigtahir@gmail.com>
2021-04-08 14:11:55 -0500

    before changing name to textInput

--- Medical Terminology/Controllers/Text Input/SingleLineInput.swift
+++ Medical Terminology/Controllers/Text Input/SingleLineInput.swift
@@ -32,10 +32,9 @@ class SingleLineInput: UIViewController, UITextFieldDelegate {
 	@IBOutlet weak var counterLabel: UILabel!
 	
 	var fieldTitle: String = "DEFAULT"
-	var inputFieldText: String = "DEFAULT"
+	var inputFieldText: String?
 	var validationText: String = "DEFAULT"
 	var validationAllowedCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz 0123456789"
-	
 	var inputIsRequired = true
 	var maxLength = 20
 	
@@ -55,7 +54,7 @@ class SingleLineInput: UIViewController, UITextFieldDelegate {
 		titleLabel.text = fieldTitle
 		textField.text = inputFieldText
 		validationLabel.text = validationText
-		updateCounterText()
+		updateAndFormatCounter()
 	}
 	
 	// MARK: - Textfield delegate methods
@@ -64,106 +63,110 @@ class SingleLineInput: UIViewController, UITextFieldDelegate {
 		return true
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	func textFieldDidChangeSelection(_ textField: UITextField) {
 		
-		let validResult = tu.validateAndFormatField(textField: textField, allowedCharacters: validationAllowedCharacters, maxLength: myConstants.maxLengthCategoryName, accessoryButton: nil)
+		let textIsValid = tu.validateAndFormatField(textField: textField, allowedCharacters: validationAllowedCharacters, maxLength: myConstants.maxLengthCategoryName, accessoryButton: nil)
 		
-		let isBlank = tu.isBlank(string: textField.text ?? "")
-		
-		switch validResult {
-		
-		case true:
-			// the input is valid
-			
-			switch inputIsRequired {
-			
-			case true:
-				// input is required
-			
-				if isBlank {
-					saveButton.isEnabled = false
-					textField.placeholder = "Required"
-				} else {
-					saveButton.isEnabled = true
-				}
-			
-			case false:
-				// input is not requried
-				
-				saveButton.isEnabled = true
-				
-				if isBlank {
-					textField.placeholder = "Optional"
-				}
-			}
-			
-		case false:
-			
-			// the input is not valid
-			saveButton.isEnabled = false
-			
-			if textField.text?.count ?? 0 > maxLength {
-				counterLabel.textColor = myTheme.invalidFieldEntryColor
-			} else {
-				textField.textColor = myTheme.colorText
-			}
-		}
+		let lengthIsValid = updateAndFormatCounter()
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		updateCounterText()
-		
-		saveButton.isEnabled = inputIsValid
-		saveButton.updateBackgroundColor()
 	}
 	
-	private func updateCounterText () {
+	private func updateAndFormatCounter () -> Bool {
 		counterLabel.text = String (maxLength - Int(textField.text?.count ?? 0))
 		
 		if textField.text?.count ?? 0 > maxLength {
 			counterLabel.textColor = myTheme.invalidFieldEntryColor
+			return false
 		} else {
 			counterLabel.textColor = myTheme.colorText
+			return true
 		}
 	}
 	
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+func textFieldDidChangeSelection(_ textField: UITextField) {
+
+let validResult = tu.validateAndFormatField(textField: textField, allowedCharacters: validationAllowedCharacters, maxLength: myConstants.maxLengthCategoryName, accessoryButton: nil)
+
+let isBlank = tu.isBlank(string: textField.text ?? "")
+
+switch validResult {
+
+case true:
+// the input is valid
+
+switch inputIsRequired {
+
+case true:
+// input is required
+
+if isBlank {
+saveButton.isEnabled = false
+textField.placeholder = "Required"
+} else {
+saveButton.isEnabled = true
+}
+
+case false:
+// input is not requried
+
+saveButton.isEnabled = true
+
+if isBlank {
+textField.placeholder = "Optional"
+}
+}
+
+case false:
+
+// the input is not valid
+saveButton.isEnabled = false
+
+if textField.text?.count ?? 0 > maxLength {
+counterLabel.textColor = myTheme.invalidFieldEntryColor
+} else {
+textField.textColor = myTheme.colorText
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+updateCounterText()
+
+saveButton.isEnabled = inputIsValid
+saveButton.updateBackgroundColor()
+}
+
+*/
