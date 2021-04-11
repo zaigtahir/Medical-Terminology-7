c4095ec70793dbfac7b73c39ae96225fe960049e
Zaigham Tahir <zaigtahir@gmail.com>
2021-04-10 19:47:16 -0500

    fixing files

---
+++ Medical Terminology/Controllers/Text Input/MultiLineInputBKBK.swift
@@ -0,0 +1,182 @@
+//
+//  TextInputVC.swift
+//  Medical Terminology
+//
+//  Created by Zaigham Tahir on 4/7/21.
+//  Copyright © 2021 Zaigham Tahir. All rights reserved.
+//
+
+import UIKit
+
+protocol MultiLineInputDelegateBKBBKK: AnyObject {
+	func shouldUpdateMultiLineInfo(inputVC: MultiLineInputVC, itemReference: String, cleanString: String)
+}
+
+class MultiLineInputVCBKBKBK: UIViewController, UITextViewDelegate {
+	
+	//
+	//  textInputVC.swift
+	//  Medical Terminology
+	//
+	//  Created by Zaigham Tahir on 4/7/21.
+	//  Copyright © 2021 Zaigham Tahir. All rights reserved.
+	//
+
+	@IBOutlet weak var headerImage: UIImageView!
+	@IBOutlet weak var titleLabel: UILabel!
+	@IBOutlet weak var inputBox: UITextView!
+	@IBOutlet weak var counterLabel: UILabel!
+	@IBOutlet weak var validationLabel: UILabel!
+	@IBOutlet weak var saveButton: ZUIRoundedButton!
+	@IBOutlet weak var cancelButton: UIButton!
+	
+	
+	var fieldTitle: String = "DEFAULT"
+	var inputFieldText: String?
+	var validationText: String = "DEFAULT"
+	var validationAllowedCharacters = "DEFAULT"
+	var inputIsRequired = true
+	var maxLength = 20
+	
+	/**
+	Use this to pass a string. The delegate will send this reference string back so the calling VC knows which value is being affected
+	*/
+	var itemReference = "DEFAULT"
+	
+	private var originalText : String?
+	
+	weak var delegate: SingleLineInputDelegate?
+	
+	
+	let tu = TextUtilities()
+	
+	override func viewDidLoad() {
+		
+		super.viewDidLoad()
+		
+		inputBox.delegate = self
+		
+		
+		//adding a tap gesture recognizer to dismiss the keyboard
+		let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing(_:)))
+		view.addGestureRecognizer(tapGesture)
+		
+		titleLabel.text = fieldTitle
+		inputBox.text = inputFieldText
+		validationLabel.text = validationText
+		
+		// backed up original to compare to the textbox.text when validating
+		originalText = inputFieldText
+		
+		// perform initial validation and set up of controls
+		
+		
+		
+		
+		
+		//textFieldDidChangeSelection(inputBox)
+		
+		
+		
+		
+		
+		
+		
+		
+		// no change is made yet as the information is just loaded, so disable the save button
+		saveButton.isEnabled = false
+		
+	}
+	
+	// MARK: - Textfield delegate methods
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+	
+	
+	
+
+	
+	
+	
+	func textFieldDidChangeSelection(_ textField: UITextField) {
+		
+		print("called textFieldDidChangeSelection")
+		
+		// check for valid characters
+		let textContainsValidCharacters = tu.textIsValid(inputString: inputBox.text ?? "", allowedCharacters: validationAllowedCharacters)
+		
+		if textContainsValidCharacters {
+			inputBox.textColor = myTheme.colorText
+		} else {
+			inputBox.textColor = myTheme.invalidFieldEntryColor
+		}
+		
+		// calling these separately. Important to do this if I want them all to run, which they won't do if I place them in a multi-and statement
+		
+		let meetsMax = meetsMaxLengthCriteria()
+		let meetsMin = meetsMinCriteria()
+		let isValid = textContainsValidCharacters
+		
+		// check to see if meeting other criteria
+		if (isValid && meetsMin && meetsMax){
+			
+			saveButton.isEnabled = true
+		} else {
+			
+			saveButton.isEnabled = false
+		}
+	
+	}
+	
+	func hasChangedFromOriginal () -> Bool {
+		let cleanText = tu.removeLeadingTrailingSpaces(string: inputBox.text ?? "")
+		if originalText ?? "" == cleanText {
+			return false
+		} else {
+			return true
+		}
+	}
+	
+	/**
+	will return true/false and also format the counter label color
+	*/
+	private func meetsMaxLengthCriteria () -> Bool {
+		
+		counterLabel.text = String (maxLength - Int(inputBox.text?.count ?? 0))
+		
+		if inputBox.text?.count ?? 0 > maxLength {
+			inputBox.textColor = myTheme.invalidFieldEntryColor
+			counterLabel.textColor = myTheme.invalidFieldEntryColor
+			return false
+		} else {
+			inputBox.textColor = myTheme.colorText
+			counterLabel.textColor = myTheme.colorText
+			return true
+		}
+		
+		
+	}
+	
+	private func meetsMinCriteria () -> Bool {
+		
+		let cleanText = tu.removeLeadingSpaces(input: inputBox.text ?? "")
+		
+		if inputIsRequired {
+			if cleanText.count > 0 {
+				return true
+			} else {
+				return false
+			}
+		} else {
+			return true
+		}
+	}
+	
+	
+	@IBAction func saveButtonAction(_ sender: Any) {
+		
+	// if the text field contains nothing, default to empty string ""
+	}
+}
--- Medical Terminology/Controllers/Text Input/MultiLineInputVC.swift
+++ Medical Terminology/Controllers/Text Input/MultiLineInputVC.swift
@@ -9,10 +9,8 @@
 import UIKit
 
 protocol MultiLineInputDelegate: AnyObject {
-	func shouldUpdateMultiLineInfo(inputVC: MultiLineInputVC, editingPropertyType: PropertyReference?, cleanString: String)
+	func shouldUpdateMultiLineInfo(inputVC: MultiLineInputVC, propertyReference: PropertyReference?, cleanString: String)
 }
-
-// shouldUpdateMultiLineInfo
 
 class MultiLineInputVC: UIViewController, UITextViewDelegate {
 	
@@ -26,32 +24,23 @@ class MultiLineInputVC: UIViewController, UITextViewDelegate {
 	
 	@IBOutlet weak var headerImage: UIImageView!
 	@IBOutlet weak var titleLabel: UILabel!
-	@IBOutlet weak var inputBox: UITextView!
+	@IBOutlet weak var textView: UITextView!
 	@IBOutlet weak var counterLabel: UILabel!
 	@IBOutlet weak var validationLabel: UILabel!
 	@IBOutlet weak var saveButton: ZUIRoundedButton!
 	@IBOutlet weak var cancelButton: UIButton!
 	
-	var fieldTitle: String = "DEFAULT"
-	var inputFieldText: String?
-	var validationText: String = "DEFAULT"
-	var validationAllowedCharacters = "DEFAULT"
-	var inputIsRequired = true
-	var maxLength = 20
-	var editingPropertyType: PropertyReference?
-	
-	private var originalText : String?
-	
+	var textInputVCH = TextInputVCH()
 	
 	weak var delegate: MultiLineInputDelegate?
 	
 	let tu = TextUtilities()
 	
 	override func viewDidLoad() {
-		
+	
 		super.viewDidLoad()
 		
-		inputBox.delegate = self
+		textView.delegate = self
 		
 		// adding a tap gesture recognizer to dismiss the keyboard
 		let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing(_:)))
@@ -59,117 +48,51 @@ class MultiLineInputVC: UIViewController, UITextViewDelegate {
 		
 		
 		// add a border to the text view
-		inputBox.layer.borderWidth = 1
-		inputBox.layer.borderColor = myTheme.colorCardBorder?.cgColor
+		textView.layer.borderWidth = 1
+		textView.layer.borderColor = myTheme.colorCardBorder?.cgColor
 		
 		
-		titleLabel.text = fieldTitle
-		inputBox.text = inputFieldText
-		validationLabel.text = validationText
-		
-		// backed up original to compare to the textbox.text when validating
-		originalText = inputFieldText
-		
+		titleLabel.text = textInputVCH.fieldTitle
+		validationLabel.text = textInputVCH.validationPrompt
+		textView.text = textInputVCH.initialText
+	
 		// perform initial validation and set up of controls
+		textViewDidChangeSelection(textView)
 		
 		// no change is made yet as the information is just loaded, so disable the save button
 		saveButton.isEnabled = false
 		
 	}
 	
-	func textViewDidChange(_ textView: UITextView) {
-		print("info changed")
-	}
-	
-	
-	/*
-	// MARK: - Textfield delegate methods
-	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-	textField.resignFirstResponder()
-	return true
-	}
-	
-	func textFieldDidChangeSelection(_ textField: UITextField) {
-	
-	print("called textFieldDidChangeSelection")
-	
-	// check for valid characters
-	let textContainsValidCharacters = tu.textIsValid(inputString: inputBox.text ?? "", allowedCharacters: validationAllowedCharacters)
-	
-	if textContainsValidCharacters {
-	inputBox.textColor = myTheme.colorText
-	} else {
-	inputBox.textColor = myTheme.invalidFieldEntryColor
-	}
-	
-	// calling these separately. Important to do this if I want them all to run, which they won't do if I place them in a multi-and statement
-	
-	let meetsMax = meetsMaxLengthCriteria()
-	let meetsMin = meetsMinCriteria()
-	let isValid = textContainsValidCharacters
-	
-	// check to see if meeting other criteria
-	if (isValid && meetsMin && meetsMax){
-	
-	saveButton.isEnabled = true
-	} else {
-	
-	saveButton.isEnabled = false
-	}
-	
-	}
-	
-	*/
-	
-	
-	func hasChangedFromOriginal () -> Bool {
-		let cleanText = tu.removeLeadingTrailingSpaces(string: inputBox.text ?? "")
-		if originalText ?? "" == cleanText {
-			return false
+	func textViewDidChangeSelection(_ textView: UITextView) {
+		// check for valid text
+		if textInputVCH.textMeetsAllCriteria(inputString: textView.text) {
+			textView.textColor = myTheme.colorText
+			saveButton.isEnabled = true
 		} else {
-			return true
+			textView.textColor = myTheme.invalidFieldEntryColor
+			saveButton.isEnabled = false
 		}
-	}
-	
-	/**
-	will return true/false and also format the counter label color
-	*/
-	private func meetsMaxLengthCriteria () -> Bool {
 		
-		counterLabel.text = String (maxLength - Int(inputBox.text?.count ?? 0))
+		// update counter
+		counterLabel.text = String (textInputVCH.maxLength - textInputVCH.getCleanText(inputString: textView.text).count)
 		
-		if inputBox.text?.count ?? 0 > maxLength {
-			inputBox.textColor = myTheme.invalidFieldEntryColor
-			counterLabel.textColor = myTheme.invalidFieldEntryColor
-			return false
-		} else {
-			inputBox.textColor = myTheme.colorText
+		if textInputVCH.meetsMaxLengthCriteria(inputString: textView.text) {
 			counterLabel.textColor = myTheme.colorText
-			return true
-		}
-		
-		
-	}
-	
-	private func meetsMinCriteria () -> Bool {
-		
-		let cleanText = tu.removeLeadingSpaces(input: inputBox.text ?? "")
-		
-		if inputIsRequired {
-			if cleanText.count > 0 {
-				return true
-			} else {
-				return false
-			}
 		} else {
-			return true
+			counterLabel.textColor = myTheme.invalidFieldEntryColor
 		}
 	}
-	
 	
 	@IBAction func saveButtonAction(_ sender: Any) {
 		
 		// if the text field contains nothing, default to empty string ""
-		delegate?.shouldUpdateMultiLineInfo(inputVC: self, editingPropertyType: self.editingPropertyType, cleanString: inputBox?.text ?? "")
+		
+		let cleanText = textInputVCH.getCleanText(inputString: textView.text)
+		
+		delegate?.shouldUpdateMultiLineInfo (inputVC: self, propertyReference: textInputVCH.propertyReference, cleanString: cleanText)
+	}
+	@IBAction func cancelButtonAction(_ sender: Any) {
+		self.navigationController?.popViewController(animated: true)
 	}
 }
