673f95bcc29a631dc113efbcb6ee9c39b632aaca
Zaigham Tahir <zaigtahir@gmail.com>
2021-04-25 10:58:03 -0500

    before changing no term cell

--- Medical Terminology/Controllers/Term List/NoTermsCell.swift
+++ Medical Terminology/Controllers/Term List/NoTermsCell.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class NoTermsCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-	@IBOutlet weak var helloLabel: UILabel!
 	
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		// Initialization code
+		cellView.layer.cornerRadius = myConstants.layout_cornerRadius
+		cellView.layer.borderWidth = 1
+		cellView.clipsToBounds = true
+		
+	}
+	
+	override func layoutSubviews() {
+		//set color here to it responds to dark mode
+		cellView.layer.borderColor = myTheme.colorCardBorder?.cgColor
+	}
+	
+	@IBOutlet weak var headingLabel: UILabel!
+	@IBOutlet weak var subheadingLabel: UILabel!
+	@IBOutlet weak var cellView: UIView!
+	
+	override func setSelected(_ selected: Bool, animated: Bool) {
+		super.setSelected(selected, animated: animated)
+		
+		// Configure the view for the selected state
+	}
+	
 }
